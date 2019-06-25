eda920e339527777123255a677a406492886c0a8
GUSTAVO CESAR MARIANO <gustavocmariano@hotmail.com>
2019-06-25 01:36:06 -0300

    Testes no item

---
+++ ProjetoEstoque.Dominio.Testes/ItemTestes.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstoque.Dominio.Testes
+{
+    [TestFixture]
+    public class ItemTestes
+    {
+        private Item _item;
+
+        [SetUp]
+        public void Inicializador()
+        {
+            _item = new Item();
+            _item.Nome = "Notebook";
+            _item.Descricao = "Samsung Essentials E20 Intel Dual Core - 4GB 500GB";
+            _item.Quantidade = 1;
+        }
+
+        [Test]
+        public void Nome_Do_Item_Nao_Pode_Ser_Vazio()
+        {
+            //ARRANGE
+            _item.Nome = string.Empty;
+
+            //ACTION
+            Action resultado = () => _item.ValidaItem();
+
+            //ASSERT
+            resultado.Should()
+                .Throw<Exception>()
+                .WithMessage("O nome não deve ser vazio!");
+        }
+
+        [Test]
+        public void Descricao_Do_Item_Deve_Ter_Mais_De_Cinco_Caracteres()
+        {
+            //ARRANGE
+            _item.Descricao = "ABCDE";
+
+            //ACTION
+            Action resultado = () => _item.ValidaItem();
+
+            //ASSERT
+            resultado.Should()
+                .Throw<Exception>()
+                .WithMessage("A descrição deve ter mais de cinco caracteres!");
+        }
+
+        [Test]
+        public void Quantidade_Do_Item_Deve_Ser_Maior_Que_Zero()
+        {
+            //ARRANGE
+            _item.Quantidade = '0';
+
+            //ACTION
+            Action resultado = () => _item.ValidaItem();
+
+            //ASSERT
+            resultado.Should()
+                .Throw<Exception>()
+                .WithMessage("A quantidade deve ser maior que zero!");
+        }
+    }
+}
--- ProjetoEstoque.Dominio/Item.cs
+++ ProjetoEstoque.Dominio/Item.cs
@@ -10,9 +10,23 @@ namespace ProjetoEstoque.Dominio
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-
         public string Descricao { get; set; }
-
         public int Quantidade { get; set; }
+
+        public void ValidaItem()
+        {
+            if (String.IsNullOrEmpty(Nome))
+            {
+                throw new Exception("O nome não deve ser vazio!");
+            }
+            else if (Descricao.Length < 6)
+            {
+                throw new Exception("A descrição deve ter mais de cinco caracteres!");
+            }
+            else if (Quantidade > 0)
+            {
+                throw new Exception("A quantidade deve ser maior que zero!");
+            }
+        }
     }
 }
